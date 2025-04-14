ecb35067773323678de045da9df692c071c48fbd
Alain Liu <luumanhquan.91@gmail.com>
2025-04-14 09:57:30 +0700

    Update example.lua

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -43,22 +43,30 @@ return {
   -- change some telescope options and a keymap to browse plugin files
   {
     "nvim-telescope/telescope.nvim",
+    -- install fzf native
+    dependencies = {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+      config = function()
+        require("telescope").load_extension("fzf")
+      end,
+    },
     keys = {
+      -- change a keymap
+      { "<C-p>", "<cmd>Telescope find_files<CR>", desc = "Find Files" },
       -- add a keymap to browse plugin files
-      -- stylua: ignore
       {
         "<leader>fp",
-        function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
+        function()
+          require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root })
+        end,
         desc = "Find Plugin File",
       },
-    },
-    -- change some options
-    opts = {
-      defaults = {
-        layout_strategy = "horizontal",
-        layout_config = { prompt_position = "top" },
-        sorting_strategy = "ascending",
-        winblend = 0,
+      -- This is using b because it used to be fzf's :Buffers
+      {
+        "<leader>b",
+        "<cmd>Telescope oldfiles<cr>",
+        desc = "Recent",
       },
     },
   },
