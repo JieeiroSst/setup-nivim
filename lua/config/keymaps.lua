0785cc809b02ca9c82ba8e7d7991e23a8f942307
Alain Liu <luumanhquan.91@gmail.com>
2025-04-14 10:11:18 +0700

    Update keymaps.lua

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+-- Go keybindings
+-- Go keybindings
+vim.keymap.set("n", "<leader>gr", "<cmd>GoRun<cr>", { desc = "Go Run" })
+vim.keymap.set("n", "<leader>gt", "<cmd>GoTest<cr>", { desc = "Go Test" })
+vim.keymap.set("n", "<leader>gd", "<cmd>GoDebug<cr>", { desc = "Go Debug" })
+vim.keymap.set("n", "<leader>gi", "<cmd>GoImport<cr>", { desc = "Go Import" })
